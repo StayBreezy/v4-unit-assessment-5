e94261b0e816617f400f8d62ae4acd7975981ccc
StayBreezy <justingross95@gmail.com>
2021-03-26 16:45:48 -0600

    limited functionality tons of bugs

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -37,8 +37,16 @@ module.exports = {
         }
       }
     },
-    createPost: (req, res) => {
-      //code here
+    createPost: async (req, res) => {
+      const {title, img, content} = req.body;
+      const db = req.app.get('db');
+      const {id} = req.session.user;
+      const date = new Date;
+      if(!id){
+        return res.status(403);
+      }
+      await db.post.create_post([+id, title, img, content, date]);
+      return res.status(200);
     },
     readPost: (req, res) => {
       req.app.get('db').post.read_post(req.params.id)
